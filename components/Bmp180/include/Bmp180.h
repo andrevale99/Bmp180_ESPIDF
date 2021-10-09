1cb74f667c85e1c8a9247c8baaec2e757afa0346
andrevale99 <andrefvale@gmail.com>
2021-10-09 15:39:01 -0300

    Acrescentado a função para obter a temperatura

--- components/Bmp180/include/Bmp180.h
+++ components/Bmp180/include/Bmp180.h
@@ -9,11 +9,13 @@
 #include "sdkconfig.h"
 #include "esp_err.h"
 
-esp_err_t setup_i2c_master(i2c_config_t *c, uint8_t pin_scl, uint8_t pin_sda);
-uint16_t bmp180_get_ut();
+esp_err_t bmp180_begin(i2c_config_t *c, uint8_t pin_scl, uint8_t pin_sda);
 
 void get_calibration_data();
 void print_calibration_data();
+
+uint32_t bmp180_get_ut();
+float bmp180_get_temperature();
 
 uint16_t read16_bits(uint16_t reg);
 esp_err_t write8_bits(uint8_t reg, uint8_t control);
