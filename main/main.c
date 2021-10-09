42c4193a15537a3ee7b81536d7115c77766880a3
andrevale99 <andrefvale@gmail.com>
2021-10-09 17:58:59 -0300

    ...

--- main/main.c
+++ main/main.c
@@ -2,6 +2,7 @@
 #include <stdbool.h>
 #include <unistd.h>
 #include <math.h>
+#include <time.h>
 
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
@@ -18,13 +19,29 @@
 void app_main(void)
 {
 
+	time_t now;
+	struct tm *data;
+
+	time(&now);
+	data = localtime(&now);
+
 	i2c_config_t conf;
 	
 	esp_err_t signal_err = bmp180_begin(&conf, I2C_SCL, I2C_SDA);
 	printf("%s ao inciar o BMP180\n\n", ((signal_err == ESP_OK)?"SUCESS":"FAIL")); 
 
 	float T = bmp180_get_temperature();
-	printf("T = %f °C\n", T);
+
+	while(true){
+		T = bmp180_get_temperature();
+		
+		printf("%s", asctime(data));
+		printf("T = %f °C\n\n", T);
+		vTaskDelay( 1000 / portTICK_PERIOD_MS);
+
+		time(&now);
+		data = localtime(&now);
+	}
 	
 	return;
 }
