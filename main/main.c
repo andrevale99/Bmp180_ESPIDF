b5cabde4456139ce0f90106597faed4c57c68873
andrevale99 <andrefvale@gmail.com>
2021-10-11 15:30:35 -0300

    Update

--- main/main.c
+++ main/main.c
@@ -16,6 +16,13 @@
 #define I2C_SCL 22
 #define I2C_SDA 21
 
+/*
+	BMP180_ULTRALOWPOWER = 0x00, //low power             mode, oss0
+	BMP180_STANDARD = 0x01,		 //standard              mode, oss1
+	BMP180_HIGHRES = 0x02,		 //high resolution       mode, oss2
+	BMP180_ULTRAHIGHRES = 0x03	 //ultra high resolution mode, oss3
+*/
+
 void app_main(void)
 {
 
@@ -24,9 +31,22 @@ void app_main(void)
 	esp_err_t signal_err = bmp180_begin(&conf, I2C_SCL, I2C_SDA);
 	printf("%s ao inciar o BMP180\n\n", ((signal_err == ESP_OK)?"SUCESS":"FAIL")); 
 
-	float T = bmp180_get_temperature();
+	set_mode_oversampling(0x03);
+
+	float T = 0;
+	float P = 0;
+
+	while(true){
+		T = bmp180_get_temperature();
+		P = bmp180_get_pressure();
+
+		printf("T = %f °C\n", T);
+		printf("P = %f Pa\n\n", P);
 		
-	printf("T = %f °C\n\n", T);
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
+	}
+
+	
 		
 	return;
 }
